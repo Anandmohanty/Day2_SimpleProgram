fc040581b0b440db1462f144886846335a645400
Anandmohanty <mohantyanand3@gmail.com>
2023-04-03 20:36:36 +0530

    [Anand] - Even And Odd

---
+++ EvenAndOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_SimpleProgram
+{
+    internal class EvenAndOdd
+    {
+        public void EvenOrOdd(int givenData, int valueTwo2)
+        {
+            if (givenData % 2 == 0)
+            {
+                Console.WriteLine("Given Number is Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Ggiven Number is Odd Number");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,23 @@
     {
         static void Main(string[] args)
         {
-            IfElse ifElse = new IfElse();
+            //IfElse ifElse = new IfElse();
             Console.WriteLine("Enter First value");
             int valueOne = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Second value");
             int valueTwo = Convert.ToInt32(Console.ReadLine());
             ifElse.EqualorNot(valueOne, valueTwo);
+
+            EvenAndOdd s1 = new  EvenAndOdd();
+            Console.WriteLine("Enter First value");
+            int valueOne1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second value");
+            int valueTwo2 = Convert.ToInt32(Console.ReadLine());
+            s1.EvenOrOdd(valueOne1, valueTwo2);
+
+
+
+
 
         }
     }
