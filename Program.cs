5c7b493197955edbfc1fefbbca710fcc5440f892
Anandmohanty <mohantyanand3@gmail.com>
2023-04-03 15:13:46 +0530

    [Anand] - Welcome simple programming

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day2_SimpleProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
