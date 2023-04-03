bb6ebf6d9a93a04071ad9320a2d97773ded419e9
Anandmohanty <mohantyanand3@gmail.com>
2023-04-03 15:23:32 +0530

    [Anand] - IfElse

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IfElse ifElse = new IfElse();
+            Console.WriteLine("Enter First value");
+            int valueOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second value");
+            int valueTwo = Convert.ToInt32(Console.ReadLine());
+            ifElse.EqualorNot(valueOne, valueTwo);
+
         }
     }
 }
